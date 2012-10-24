4aa37e8448c194266926a8f60994b4ec86a83554
Adierivot <Adierivot@ieee.org>
2012-10-24 03:38:33 -0400

    Test class complete

---
+++ tester.java
@@ -0,0 +1,16 @@
+/**
+* This is the test class for the other classes 
+* It creates a playNim Object then runs the play method to run the game
+*/
+
+/**
+*@author Ashley Dierivot
+*/
+
+public class tester{
+	public static void main(String[]args){
+		PlayNim game = new PlayNim();
+			game.play();
+			}
+			
+}

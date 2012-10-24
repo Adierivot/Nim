b51b9de3e7bd58f54fbd9a920e21c319ef850268
Adierivot <Adierivot@ieee.org>
2012-10-24 18:28:30 -0400

    Added documentation

--- Pile.java
+++ Pile.java
@@ -1,8 +1,20 @@
 import java.util.Random; 
 
+/**
+* This is the pile class that is to be used in the game of nim.
+* It delegates the random number of marbles at the start of each game
+* and updates the pile when needed 
+*
+* @author Ashley Dierivot
+*/
+
 public class Pile {
 	
-	public int marbles;
+	public int marbles;		//the amount of marbles in the pile
+	
+	/**
+	* Creates a Pile object
+	*/
 	
 	public pile()
 	{
@@ -10,12 +22,20 @@ public class Pile {
 		marbles = rand.nextInt(91)+10;
 	}
 	
+	/**
+	* The amount of marbles in the pile
+	* @return the amount of marbles in the pile
+	*/
 	public int getSize()
 	{
 		return marbles;
 	}
 	
-	
+	/**
+	* The amount of marbles in the pile
+	* @param removed the umber of marbles removed
+	* @return the updated amount of marbles in the pile
+	*/
 	public int updatedSize(int removed)
 	{
 		marbles = marbles - removed;
